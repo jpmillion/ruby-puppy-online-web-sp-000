029cf6548c4ffd296f11d9dcda0480c551d66b00
John Million <jpmillion@comcast.net>
2020-11-16 21:32:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,16 @@ class Dog
   attr_accessor :name 
   @@all = []
   
+  def self.save
+    @@all << self
+  end
+  
   def initialize(name)
     @name = name
     self.save
   end
   
-  def self.save
-    @@all << self
-  end
+
   
   def self.all
     @@all
